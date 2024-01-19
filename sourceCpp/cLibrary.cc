8cff5935890095b90a2730ee1737a40f559ce4d7
cj05 <cjcoolsuper@gmail.com>
2024-01-19 23:10:21 +0700

    cpp snapshot save

--- sourceCpp/cLibrary.cc
+++ sourceCpp/cLibrary.cc
@@ -1,4 +1,7 @@
 #include <napi.h>
+#include "csv.hpp"
+
+using namespace csv;
 
 Napi::Value SumOfN(const Napi::CallbackInfo &info)
 {
@@ -7,6 +10,35 @@ Napi::Value SumOfN(const Napi::CallbackInfo &info)
     for (double i = 0; i < LIMIT; i++)
         sum += i;
     Napi::Number res = Napi::Number::New(info.Env(), sum);
+    return res;
+}
+
+Napi::Value SimpleSuggestion(const Napi::CallbackInfo &info)
+{
+    std::string ModelFilePath = info[0].ToString().Utf8Value();
+    std::string ModelBigData = info[1].ToString().Utf8Value();
+    Napi::Array Sample = info[2].As<Napi::Array>();//User data
+    //format, open file or whatever
+    CSVReader reader("very_big_file.csv");
+    for (CSVRow& row: reader) { // Input iterator
+    for (CSVField& field: row) {
+        // By default, get<>() produces a std::string.
+        // A more efficient get<string_view>() is also available, where the resulting
+        // string_view is valid as long as the parent CSVRow is alive
+        std::cout << field.get<>() << ...
+    }
+}
+
+    
+
+    //Loop over data
+
+
+
+    
+
+
+    Napi::Number res = Napi::Number::New(info.Env(), 0);
     return res;
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+
 import Card from '../../assets/Components/cards';
 import styles from './HomeComponent.module.scss';
 const Home = () => {
   return (
     <div>
       <Card/>
+      Hi
     </div>
   );
 };
